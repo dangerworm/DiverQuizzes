e4c460338819a4f5143746acf49ce973bb1c2fe3
dangerworm <dangerworm@gmail.com>
2024-11-15 08:06:16 +0000

    Updates

--- bsac-library/fetch-quizzes.js
+++ bsac-library/fetch-quizzes.js
@@ -53,27 +53,69 @@ function parseQuestions(html) {
   return questions;
 }
 
-function parseAnswerCode(html) {
+async function downloadImage(imageUrl, outputPath) {
+  return new Promise((resolve, reject) => {
+    https.get(`${BASE_URL}${imageUrl}`, (response) => {
+      if (response.statusCode !== 200) {
+        reject(new Error(`Failed to download image ${imageUrl}: ${response.statusCode}`));
+        return;
+      }
+
+      const fileStream = fs.createWriteStream(outputPath);
+      response.pipe(fileStream);
+      
+      fileStream.on('finish', () => {
+        fileStream.close();
+        resolve();
+      });
+    }).on('error', reject);
+  });
+}
+
+function parseAnswerCode(html, quizType, quizNumber) {
   const answersStart = html.indexOf(`I=new Array();`);
   const answersEnd = html.indexOf(`function StartUp(){`);
   const lines = html.substring(answersStart, answersEnd).split('\n');
 
+  const imagesDir = path.join(__dirname, 'images');
+  if (!fs.existsSync(imagesDir)) {
+    fs.mkdirSync(imagesDir);
+  }
+
   const codeRegex = /(.*)new Array\((.*)\)(.*)/;
   let answerCode = "";
+  let downloadPromises = [];
+
   for (let i = 1; i < lines.length; i++) {
     const line = lines[i].trim();
-    const codeMatches = line.match(codeRegex);
+    
+    const imgRegex = /<img[^>]+src="([^"]+)"[^>]*>/g;
+    let imgMatch;
+    
+    while ((imgMatch = imgRegex.exec(line)) !== null) {
+      const imageSrc = imgMatch[1];
+      const imageFileName = imageSrc.toLowerCase();
+      const localImagePath = path.join(imagesDir, `${quizType.prefix}${quizNumber}_${imageFileName}`);
+      
+      const newImgSrc = `/bsac-library/images/${quizType.prefix}${quizNumber}_${imageFileName}`;
+      const updatedLine = line.replace(imageSrc, newImgSrc);
+      
+      downloadPromises.push(downloadImage(imageSrc, localImagePath));
+      
+      answerCode += `    ${updatedLine}\n`;
+      continue;
+    }
 
+    const codeMatches = line.match(codeRegex);
     if (!codeMatches) {
       answerCode += `    ${line}\n`;
-    }
-    else {
+    } else {
       const code = `${codeMatches[1]}[${codeMatches[2]}]${codeMatches[3]}`
       answerCode += `    ${code}\n`;
     }
   }
 
-  return answerCode.trim();
+  return { answerCode: answerCode.trim(), downloadPromises };
 }
 
 async function processQuiz(quizType, quizNumber) {
@@ -83,7 +125,13 @@ async function processQuiz(quizType, quizNumber) {
   try {
     const html = await fetchHtml(url);
     const questions = parseQuestions(html);
-    const answerCode = parseAnswerCode(html);
+    const { answerCode, downloadPromises } = parseAnswerCode(html, quizType, quizNumber);
+
+    if (downloadPromises.length > 0) {
+      console.log(`Downloading ${downloadPromises.length} images...`);
+      await Promise.all(downloadPromises);
+      console.log('Images downloaded successfully');
+    }
 
     const outputPath = path.join(__dirname, `${quizType.prefix}${quizNumber}.js`.toLowerCase());
     const fileContent = `module.exports = {
@@ -93,7 +141,7 @@ async function processQuiz(quizType, quizNumber) {
 
   getAnswers: () => {
     let I = [];
-    ${(answerCode)}
+    ${answerCode}
     return I;
   }
 };`;
